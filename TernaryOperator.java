0e136c8b6539d653360572b6678587e79ba94bde
shravyaevangileena <99160968+shravyaevangileena@users.noreply.github.com>
2022-02-07 11:17:50 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,14 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+	int v1=5;
+	int v2=10;
+	int r;
+	boolean condition= true;
+	r= condition ? v1 : v2;
+	System.out.println(r);
+
+	}
+
+} 
